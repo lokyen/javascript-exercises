f47805f2631dae8115acf4313d6546214dfec7dd
lokyen <lokyen1225@gmail.com>
2024-04-17 15:28:01 -0400

    practiced tdd testing

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let start; 
+    let end;
+    if(typeof num1 != 'number' || typeof num2 != 'number' || num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    if(num1 < num2){
+        start = num1;
+        end = num2;
+    }else{
+        start = num2;
+        end = num1;
+    }
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
