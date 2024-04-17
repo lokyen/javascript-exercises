da4691961643be27416d16960b0784553120c94e
lokyen <lokyen1225@gmail.com>
2024-04-17 15:14:24 -0400

    practiced using jest and testing

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let letters = word.split("");
+    let reverseWord = "";
+    for(let i = word.length - 1; i >= 0; i--){
+        reverseWord += letters[i];
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
